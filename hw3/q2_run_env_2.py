a6aaee1831392709f6964553e8add04f65dc1bd4
Adnan Shahpurwala <ashahpur@eng.ucsd.edu>
2019-11-06 21:11:14 -0800

    final commit. All working! :D

--- hw3/q2_run_env_2.py
+++ hw3/q2_run_env_2.py
@@ -11,22 +11,30 @@ class Policy(nn.Module):
 
     def __init__(self, num_classes, lr):
         super(Policy, self).__init__()
-        self.fc1 = nn.Linear(8, 128)
-        self.fc2 = nn.Linear(128, num_classes)
+        
+        self.sigma = nn.parameter.Parameter(torch.tensor([[0.1, 0.], [0., 0.1]]))
+        
+        self.fc1 = nn.Linear(8, 64)
+        self.hidden = nn.Linear(64, 64)
+        self.fc2 = nn.Linear(64, num_classes)
         self.tanh = nn.Tanh()
-
-        self.sigma = nn.Parameter(torch.tensor([0.1, 0.1]))
         
         self.optimizer = optim.Adam(self.parameters(), lr=lr)
         
     def forward(self, x):
         
-        x = self.fc1(x)
+        x = self.fc1(x) #input
         x = self.tanh(x)
-        x = self.fc2(x)
+        x = self.hidden(x) #layer 1
         x = self.tanh(x)
-                
-        return x
+        x = self.hidden(x) #layer 2
+        x = self.tanh(x)
+        x = self.fc2(x) #layer 3
+        x = self.tanh(x)
+
+        sigma = torch.relu(self.sigma) + torch.tensor([[0.001, 0.], [0., 0.001]])
+
+        return x, sigma
 
 def makePolicyNN(num_actions=2, lr=0.01):
     ''' Initialize the policy class '''
@@ -39,10 +47,10 @@ def getAction(self, policy_network, state):
     assert isinstance(state, np.ndarray)
     
     state = torch.from_numpy(state).float()
-    torque_mean = policy_network(state) #forward pass
+    torque_mean, torque_sigma = policy_network(state) #forward pass
     
     # Sampling from the 2D Gaussian and calculating the actions log probability
-    m = MultivariateNormal(torque_mean, torch.abs(torch.diag(policy_network.sigma + 1e-3)))
+    m = MultivariateNormal(torque_mean, torque_sigma)
     action = m.sample() #type tensor
     log_prob_of_action = m.log_prob(action) #type tensor
 
@@ -50,7 +58,7 @@ def getAction(self, policy_network, state):
 
 if __name__ == '__main__':
 
-    policy_network_file = './' + 'q2_policy3' + '.pth.tar'
+    policy_network_file = './' + 'q2_policy4' + '.pth.tar'
     policy_network = makePolicyNN(num_actions=2, lr=1e-3)
 
     print(' =======> Loading model from file ', policy_network_file)
@@ -64,14 +72,29 @@ if __name__ == '__main__':
     env.render('human')
     state = env.reset()
     done = False
-    time.sleep(1)
-    while steps<300:
+    time.sleep(3)
+    while not(done):
         state = torch.from_numpy(state).float()
-        a = policy_network(state) #forward pass
+        a, _ = policy_network(state)
         state, r, done, info = env.step(np.array(a.data))
         steps+=1
         env.render('human')
-        time.sleep(0.05)
+        time.sleep(0.2)
     print('Total steps = ', steps)
     env.env.close()
+
+    # env = gym.make("modified_gym_env:ReacherPyBulletEnv-v1", rand_init=False)
+    # steps = 0
+    # env.render('human')
+    # state = env.reset()
+    # done = False
+    # time.sleep(1)
+    # while steps<200:
+    #     a, _ = policy_network(state) #forward pass
+    #     state, r, done, info = env.step(np.array(a))
+    #     steps+=1
+    #     env.render('human')
+    #     time.sleep(0.05)
+    # print('Total steps = ', steps)
+    # env.env.close()
 
